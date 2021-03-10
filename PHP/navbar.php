99d2a00adc4ab11d0c826dc0d64b538c4c2cad90
SumitBaria <imsumitbaria@gmail.com>
2021-03-10 11:51:03 +0530

    Readdy for deploy

--- PHP/navbar.php
+++ PHP/navbar.php
@@ -8,7 +8,7 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item "><a class="nav-link" href="Home.php">Home</a></li>
+          <li class="nav-item "><a class="nav-link" href="index.php">Home</a></li>
           <li class="nav-item "><a class="nav-link" href="transfer.php">Transfer Money</a></li>
           <li class="nav-item "><a class="nav-link" href="transactionhistory.php">Transaction History</a></li>
         </ul>
