1a0fd460c72e076bdbac0951ce93d7ee5b3ce4c2
SumitBaria <imsumitbaria@gmail.com>
2021-03-10 11:41:02 +0530

    Readdy for deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7baf626e1399ae048d58085e4663cc7b75da8c91',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7baf626e1399ae048d58085e4663cc7b75da8c91',
+    ),
+  ),
+);
