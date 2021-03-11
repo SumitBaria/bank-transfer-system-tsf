bac194f0d95693568bf1219379c4dbfc392106be
SumitBaria <imsumitbaria@gmail.com>
2021-03-11 20:20:35 +0530

    Ready  for deploy

--- PHP/config.php
+++ PHP/config.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('remotemysql.com','an5XkSGi7t','MFkuLomlc3','oyjnDeuZIQ');
+	$conn = mysqli_connect('remotemysql.com','oyjnDeuZIQ','1vADy0lQqY','oyjnDeuZIQ');
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
