41d3bd280eb456af2dc9ec951cd407015a5bc009
SumitBaria <imsumitbaria@gmail.com>
2021-03-11 20:18:57 +0530

    Ready  for deploy

--- PHP/config.php
+++ PHP/config.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('remotemysql.com','an5XkSGi7t','MFkuLomlc3','BankSystem');
+	$conn = mysqli_connect('remotemysql.com','an5XkSGi7t','MFkuLomlc3','oyjnDeuZIQ');
 
 	if(!$conn){
 		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
